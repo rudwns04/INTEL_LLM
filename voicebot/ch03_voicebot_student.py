12fd8f5909f8fa1316577869a2c6a83f09400e97
rudwns04 <154478954+rudwns04@users.noreply.github.com>
2024-02-22 14:47:40 +0900

    Add files via upload

--- voicebot/ch03_voicebot_student.py
+++ voicebot/ch03_voicebot_student.py
@@ -78,19 +78,19 @@ def main():
 
 
     #제목
-    st.header(" ")
+    st.header(" 음성 비서 프로그램 ")
 
     #구분선
     st.markdown("---")
 
     # 기본 설명
-    with st.expander("EY Bōt", expanded=True):
+    with st.expander("음성비서 프로그램에 관하여", expanded=True):
         st.write(
             '''
             - 음성비서 프로그램의 UI는 스트림릿을 활용했습니다.
             - STT(Speech-To-Text)는 OpenAI의 Whisper AI를 활용했습니다.
             - 답변은 OpenAI의 GPT 모델을 활용했습니다.
-            - TTS (Text-To-Speech) 72 Google Translate TTS
+            - TTS (Text-To-Speech) 72 Google Translate TTS를 활용했습니다.
 
             '''
         )
@@ -101,17 +101,17 @@ def main():
     with st.sidebar:
         
         #Open AI API 키 입력받기
-        openai.api_key = st.text_input(label="OPENAI API |", placeholder="Enter Your API Key", value="", type="password")
+        openai.api_key = st.text_input(label="OPENAI API 키", placeholder="Enter Your API Key", value="", type="password")
 
         st.markdown("---")
 
         # GPT 모델을 선택하기 위한 라디오 버튼 생성
-        model=st.radio(label="GPT",options=["gpt-4", "gpt-3.5-turbo"])
+        model=st.radio(label="GPT 모델",options=["gpt-4", "gpt-3.5-turbo"])
 
         st.markdown("---")
 
     # 리셋 버튼 생성
-    if st.button(label="7|3|"): 
+    if st.button(label="초기화"): 
         #리셋 코드
         st.session_state["chat"] = []
         st.session_state["messages"] = [{"role": "system", "content": "You are a thoughtful assistant.\
